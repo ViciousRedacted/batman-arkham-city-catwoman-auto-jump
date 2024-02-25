f52c37e1475cd29329ecc2173ef648d8bcea3af2
ViciousRedacted <124419468+ViciousRedacted@users.noreply.github.com>
2024-02-24 22:25:26 -0800

    Add files via upload

---
+++ Start.sh
@@ -0,0 +1,13 @@
+@echo off
+export /p firststart=< Assets\FirstStart.ini
+if "${firststart}"=="1" (goto installpkg) else (goto start)
+
+:installpkg
+echo it seems to be your first time running this program
+echo installing dependencies...
+pip install pyautogui < nul
+pip install os < nul
+
+:start
+echo auto jump started close this window to stop
+python "Assets\BACautoJump.py"
